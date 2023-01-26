ff14bb36143c72271176e98797144ceb6963ce5f
guillaumehu <guillaumehuguet2@gmail.com>
2023-01-26 14:04:24 -0500

    anon branch

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     packages=find_packages(),
     version='0.1.0',
     description='Fisher Information Metric with PHATE',
-    author='Guillaume Huguet',
+    author='anon anon',
     license='MIT',
 )
